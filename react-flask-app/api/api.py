a9db6e6eb605263486e8633475bcb9e334d80999
Nick Battista <halodude3675@gmail.com>
2023-03-16 22:57:40 -0400

    Data properly displays on frontend
    after tedious and grueling work, the data now properly displays on the front end and is dynamic to the database.

--- react-flask-app/api/api.py
+++ react-flask-app/api/api.py
@@ -1,8 +1,55 @@
-import time
-from flask import Flask
+import time, os
+from flask import Flask, render_template, request, url_for, redirect, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+from dataclasses import dataclass
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.debug = True
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+@dataclass
+class Profile(db.Model):
+    # Id : Field which stores unique id for every row in
+    # database table.
+    # first_name: Used to store the first name if the user
+    # last_name: Used to store last name of the user
+    # Age: Used to store the age of the user
+    school_id:int = db.Column(db.Integer, primary_key=True)
+    name:str = db.Column(db.String(20), unique=False, nullable=False)
+    points:int = db.Column(db.Integer, nullable=False)
+ 
+    # repr method represents how one object of this datatable
+    # will look like
+    # def __repr__(self):
+    #     return f"Name : {self.first_name}, Age: {self.age}"
+
+@app.route('/data')
+def get_data():
+    students = Profile.query.all()
+    # for row in students:
+        # string += {
+        #     "Name":row.name,
+        #     "School_id":row.school_id,
+        #     "Points": row.points
+        # }
+        # return {
+        #     "Name":row.name,
+        #     "School_id":row.school_id,
+        #     "Points": row.points
+        # }
+    return jsonify(students)
+
+# Might be able to delete below method
+# @app.route('/')
+# def index():
+#     students = Profile.query.all()
+#     for row in students:
+#         return{"Fname: ",row.first_name, "Lname:",row.last_name, "SchId:", row.school_id, "Age:", row.age}
+    # return render_template('index.html', students=students)
--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -1,32 +1,71 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
+  // const [data, setdata] = useState({
+  //   name: "",
+  //   school_id: 0,
+  //   points: 0
+  // });
+
+  // useEffect(() => {
+  //   fetch("/data").then((res) =>
+  //     res.json().then((data) => {
+  //       setdata({
+  //         name: data.name,
+  //         school_id: data.school_id,
+  //         points: data.points,
+  //       });
+  //     })
+  //   );
+  // }, []);
+
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
+    fetch('/data')
+    .then(res => res.json())
+    .then(setData)
+    .catch(console.error);
   }, []);
+
+  // getFetchUsers() {
+  //   this.setState({
+  //       loading: true
+  //   }, () => {
+  //       fetch("http://localhost:3000/data").then(res => res.json()).then(result => this.setState({
+  //           loading: false,
+  //           users: result
+  //       })).catch(console.log);
+  //   });
+  // }
+  // componentDidMount() {
+  //     this.getFetchUsers();
+  // }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is {currentTime}.</p>
+        <h1>Registered Students:</h1>
+        
+        {data.map((user) => (
+          <div>
+            <table style={{border:"1px solid black"}}>
+              <tr>
+                <th style={{width:"10em",border:"1px solid black"}}>Name</th>
+                <th style={{width:"3em", border:"1px solid black"}}>Id</th>
+                <th style={{width:"5em", border:"1px solid black"}}>Points</th>
+              </tr>
+
+              <tr>
+                <td style={{textAlign:"center", border:"1px solid black"}}>{user.name}</td>
+                <td style={{textAlign:"center", border:"1px solid black"}}>{user.school_id}</td>
+                <td style={{textAlign:"center", border:"1px solid black"}}>{user.points}</td>
+              </tr>
+            </table>
+          </div>
+        ))}
+        
       </header>
     </div>
   );
